bd28769b17359402f60aa94d14f72aa3be10551a
D'André Phillips <32004621+ibvsmxn@users.noreply.github.com>
2021-06-29 03:09:19 +0100

    Complete work

---
+++ the-profs/main.js
@@ -0,0 +1,8 @@
+// NAVIGATION CHANGE COLOUR ON SCROLL
+window.onscroll = () => {
+    const target = document.body;
+    if(this.scrollY <= 50) target.className = ''; else target.className = 'scroll';
+    // if(this.scrollY > 4750) target.className = 'end';
+
+    // console.log(this.scrollY);
+};
